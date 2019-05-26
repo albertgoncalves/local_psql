5191f3c57670a320aa3d29bc8b37f594de5aef16
albertgoncalves <goncalves.albert@gmail.com>
2019-05-25 20:39:33 -0400

    higher-order safety

--- scripts/example.py
+++ scripts/example.py
@@ -14,24 +14,50 @@ def connect(kwargs):
     return create_engine(url(kwargs)).connect()
 
 
-def ops(con):
-    sql = "SELECT * FROM t1;"
-    print(read_sql(sql, con))
+def try_con(credentials):
+    try:
+        return (connect({k: environ[v] for (k, v) in credentials}), None)
+    except Exception as e:
+        return (None, e)
+    finally:
+        print("ESTABLISHED CONNECTION")
+
+
+def try_effects(f, con):
+    try:
+        f(con)
+        error = None
+    except Exception as e:
+        error = e
+    finally:
+        con.close()
+        print("\nCLOSED CONNECTION")
+        return error
+
+
+def pipeline(f, credentials):
+    con, error = try_con(credentials)
+    if not error:
+        error = try_effects(f, con)
+    if error:
+        print("\n{}".format(error))
 
 
 def main():
-    creds = {
+    def effects(con):
+        sql = "SELECT * FROM t1;"
+        data = read_sql(sql, con)
+        print("COMPLETED QUERY\n")
+        print(data)
+
+    credentials = {
         ("user", "PGUSER"),
         ("host", "PGHOST"),
         ("port", "PGPORT"),
         ("db", "PGDB"),
         ("password", "PGPASS"),
     }
-    con = connect({k: environ[v] for (k, v) in creds})
-    try:
-        ops(con)
-    finally:
-        con.close()
+    pipeline(effects, credentials)
 
 
 if __name__ == "__main__":
